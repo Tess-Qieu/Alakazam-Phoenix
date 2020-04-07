cde578e418e471f42d15a4d2a6a6c844ee305ac5
Reituag <gauthierhobbes@free.fr>
2020-04-07 19:14:08 +0200

    Cell selection
    Change of material on cell click (floor only)

--- HexGrid/Prefabs/Cell/Cell.gd
+++ HexGrid/Prefabs/Cell/Cell.gd
@@ -4,6 +4,11 @@ var q
 var r
 var kind
 
+# Array of materials
+# 0 = unselected
+# 1 = selected
+var materials = []
+
 const CIRCLE_RAY = 1
 const SPACE_BETWEEN = 0.05
 const DIST = sqrt(3)*CIRCLE_RAY
@@ -13,7 +18,7 @@ const TRANS_RIGHT = [DIST*RATIO, 0]
 const TRANS_DOWNRIGHT = [DIST*RATIO/2, 3.0*CIRCLE_RAY*RATIO/2]
 
 func _ready():
-	pass 
+	pass
 
 func init(_q, _r, _kind, _height):
 	q = _q
@@ -23,12 +28,18 @@ func init(_q, _r, _kind, _height):
 	translation.x = q * TRANS_RIGHT[0] + r * TRANS_DOWNRIGHT[0]
 	translation.y = _height * 0.5
 	translation.z = r * TRANS_DOWNRIGHT[1]
+	
+	materials.append(load("res://Prefabs/Cell/Cell_White/Faces.material"))
+	materials.append(load("res://Prefabs/Cell/selected_mat.tres"))
+	
 
 
 func _on_Area_input_event(camera, event, click_position, click_normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed == true:
 			print('Cell {0} / {1} clicked !'.format([q, r]))
+			if kind == "floor":
+				$Circle.set_surface_material(0, materials[1])
 	
 	elif event is InputEventMouseMotion:
 		print('Cell {0} / {1} motionned !'.format([q, r]))
