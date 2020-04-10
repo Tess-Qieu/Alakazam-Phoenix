076d18b3f1c49428858abb52353e28908c910634
Yannis Bendi-Ouis <yannis.bendiouis@gmail.com>
2020-04-10 18:50:17 +0200

    modif changement de couleur"

--- HexGrid/Scripts/Cell.gd
+++ HexGrid/Scripts/Cell.gd
@@ -3,6 +3,7 @@ extends Spatial
 var q
 var r
 var kind
+var origin_material
 
 const CIRCLE_RAY = 1
 const SPACE_BETWEEN = 0
@@ -12,31 +13,29 @@ const RATIO = (DIST + SPACE_BETWEEN)/DIST
 const TRANS_RIGHT = Vector2(DIST*RATIO, 0)
 const TRANS_DOWNRIGHT = Vector2(DIST*RATIO/2, 3.0*CIRCLE_RAY*RATIO/2)
 
-const color_kind = {'hole': 'ae8257', 'floor': "e6cab8", 'full': '352f2b', \
-	'border':'352f2b'}
-
 func _ready():
 	pass 
 
-func init(_q, _r, _kind):
-	q = _q
-	r = _r
-	kind = _kind
+func init(q, r, kind, material):
+	self.q = q
+	self.r = r
+	self.kind = kind
+	self.origin_material = material
 	translation.x = q * TRANS_RIGHT.x + r * TRANS_DOWNRIGHT.x
 	translation.z = r * TRANS_DOWNRIGHT.y
-	change_color(color_kind[kind])
+	change_material(material)
 
-func change_color(color):
-	var material = $Circle.get_surface_material(0)
-	material.albedo_color = color
-#	$Circle.set_surface_material(0, material)
+func change_material(material):
+	$Circle.set_surface_material(0, material)
 
 func _on_Area_input_event(_camera, event, _click_position, _click_normal, _shape_idx):
 	# Catch click & motion events on cell
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed == true:
 			# cell clicked
-			print('Cell {0} / {1} : {2} !'.format([q, r, kind]))
+			var mat = SpatialMaterial.new()
+			mat.albedo_color = Color(1, 0, 0)
+			change_material(mat)
 	
 	elif event is InputEventMouseMotion:
 #		print('Cell {0} / {1} motionned !'.format([q, r]))
