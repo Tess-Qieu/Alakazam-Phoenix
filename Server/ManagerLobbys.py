c4792b4e0c4a9432d584f6523627c25fbb8706f2
Yannis Bendi-Ouis <yannis.bendiouis@gmail.com>
2020-04-16 13:17:06 +0200

    Init Lobby

---
+++ Server/ManagerLobbys.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+class ManagerID():
+    """ Keep in memory all free games ids."""
+    
+    def __init__(self, N=1000000):
+        self.ids = list(range(N))
+
+    def get_new_id():
+        return self.ids.pop(0)
+
+    def free_id(id):
+        self.ids += [id]
+
+
+class ManagerLobbys():
+    """ Used by the server to manage all lobbys. """
+
+    def __init__(self):
+        self.lobbys = {}
+        self.manager_id = ManagerID()
+        self.players_waiting_to_play = []
+
+
+    def treat_request():
+        pass
--- Server/Server.py
+++ Server/Server.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-# WS server example
 
 import asyncio
 import websockets
