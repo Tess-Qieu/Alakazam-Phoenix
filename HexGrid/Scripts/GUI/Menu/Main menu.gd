1d9927c9f6884598e0962f342a9f5a82a6fcfe6d
Reituag <gauthierhobbes@free.fr>
2020-05-09 11:43:43 +0200

    Update of main menu

--- HexGrid/Scripts/GUI/Menu/Main menu.gd
+++ HexGrid/Scripts/GUI/Menu/Main menu.gd
@@ -1,16 +1,14 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Background animation 
 	$Spatial/AnimationPlayer.play("round")
+	
+	# Connection of name validation
+	$VBoxContainer/Name_Selector.connect("text_entered", self, "_name_selection")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _name_selection(new_text):
+	# /!\ temporary print, should be used to save player's name 
+	print ("Hello " + new_text)
