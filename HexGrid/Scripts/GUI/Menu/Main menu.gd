4eca3373610b6095b384a5c149c16ed296b3b8bc
Reituag <gauthierhobbes@free.fr>
2020-05-09 12:07:40 +0200

    Connection of button with games

--- HexGrid/Scripts/GUI/Menu/Main menu.gd
+++ HexGrid/Scripts/GUI/Menu/Main menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var localGame = preload("res://Scenes/BattleScreen.tscn")
+var onlineGame = preload("res://Scenes/Game.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,7 +10,17 @@ func _ready():
 	
 	# Connection of name validation
 	$VBoxContainer/Name_Selector.connect("text_entered", self, "_name_selection")
+	# Start local game connection
+	$HBoxContainer/Local_Button.connect("pressed", self, "_goto_local_game")
+	# Start online game connection
+	$HBoxContainer/Online_Button.connect("pressed", self, "_goto_online_game")
 
 func _name_selection(new_text):
 	# /!\ temporary print, should be used to save player's name 
 	print ("Hello " + new_text)
+
+func _goto_local_game():
+	get_tree().change_scene_to(localGame)
+
+func _goto_online_game():
+	get_tree().change_scene_to(onlineGame)
