ab7bd8946b36e543eba3a84d1079f3569914b036
Yannis Bendi-Ouis <yannis.bendiouis@gmail.com>
2021-01-17 17:12:21 +0100

    change color character

--- HexGrid/Scripts/Characters/Robot_character.gd
+++ HexGrid/Scripts/Characters/Robot_character.gd
@@ -1,11 +1,15 @@
 extends 'res://Scripts/Characters/Character.gd'
 
+var color1 = 'fab45a' #yellow
+var color2 = '646464'   #grey
+
 func _ready():
 	speed = 150
 	
 	# Miniature update
 	miniature = load("res://Prefabs/Character/Robot_miniature.png")
-
-
+	
 func change_material(material_key: String):
 	$Armature/TorsoBoneAttachment/BodyTorso.set_surface_material(0, Global.materials[material_key])
+#	$Armature/AntennaLBoneAttachment/Antenna_L.set_surface_material(0, Global.materials[material_key])
+#	$Armature/AntennaRBoneAttachment/Antenna_R.set_surface_material(0, Global.materials[material_key])
