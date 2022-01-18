b2752b99f09d7adc7466022b3a1af795baf330ae
Reituag <gauthierhobbes@free.fr>
2022-01-18 22:50:47 +0100

    New font & MainMenu rework
    Addition of a font for the projet
    Revision of main menu after upscaling + better organization

--- HexGrid/Scripts/GUI/Menu/MainMenu.gd
+++ HexGrid/Scripts/GUI/Menu/MainMenu.gd
@@ -1,6 +1,8 @@
 extends Control
 
-
+onready var local_bt  = $Visual_components/VBoxContainer2/HBoxContainer/Local_Button
+onready var online_bt = $Visual_components/VBoxContainer2/HBoxContainer/Online_Button
+onready var name_box  = $Visual_components/Name_Container/Name_Selector
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,13 +10,13 @@ func _ready():
 	$Spatial/AnimationPlayer.play("round")
 	# Connection of name validation
 	# warning-ignore:return_value_discarded
-	$VBoxContainer/Name_Selector.connect("text_changed", self, "_name_selection")
+	name_box.connect("text_changed", self, "_name_selection")
 	# Start local game connection
 	# warning-ignore:return_value_discarded
-	$HBoxContainer/Local_Button.connect("pressed", self, "_goto_local_game")
+	local_bt.connect("pressed", self, "_goto_local_game")
 	# Start online game connection
 	# warning-ignore:return_value_discarded
-	$HBoxContainer/Online_Button.connect("pressed", self, "_goto_online_game")
+	online_bt.connect("pressed", self, "_goto_online_game")
 
 func _name_selection(new_text):
 	Global.pseudo = new_text
