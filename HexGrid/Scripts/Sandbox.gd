120b62e2dc670b7a0e785f1258ffaf7a1f36f85d
Reituag <gauthierhobbes@free.fr>
2022-02-05 23:16:16 +0100

    MouseEvents through viewport addition
    Adding a solution from this link https://github.com/godotengine/godot/issues/17326

--- HexGrid/Scripts/Sandbox.gd
+++ HexGrid/Scripts/Sandbox.gd
@@ -7,6 +7,7 @@ onready var MenuPopup : PopupMenu
 const MapClass = preload("res://Scenes/Map.tscn")
 
 # Children references
+onready var viewportContainer = $VBoxContainer/HBoxContainer/Viewport_Container
 onready var myWorldRoot = $VBoxContainer/HBoxContainer/Viewport_Container/myWorldRoot
 var myMap = null
 
@@ -15,6 +16,8 @@ var world : Dictionary = {}
 
 # Path to world file
 var worldSave : File = null
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -55,7 +58,7 @@ func newWorld():
 	myMap = MapClass.instance()
 	# Initialize new map
 	# Generate map
-	myMap.generate_grid()
+	myMap.generate_grid(true)
 	myMap.instance_map(myMap.grid)
 	# Add map
 	myWorldRoot.add_child(myMap)
